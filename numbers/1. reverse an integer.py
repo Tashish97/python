ffbb57bd6a1103c01d81267f8823dbc5b0464bed
Ashish Toppo <toppo.ashish7@gmail.com>
2023-04-18 08:31:22 +0530

    added recrsion part to reversing a given integer

--- numbers/1. reverse an integer.py
+++ numbers/1. reverse an integer.py
@@ -1,6 +1,19 @@
 num = int(input('Enter a number:  '))
 rev = 0
+
+def reverse_recursion(num):
+    '''
+    A function to reverse a number with recursion
+    '''
+    if num<10:
+        return num
+    else:
+        return int(str(num%10) + str(reverse_recursion(num//10)))
+rec = reverse_recursion(num)
+print("With Recursion: ",rec)
+
+# solving recursion with iteration
 while num!=0:
     rev = rev*10 + num%10
     num = num//10
-print(rev)
+print("With Iteration: ",rev)
